75b335bb7cd59ec7ed7f211c9378362618f84116
n0310441 <n0310441@VDDP03C-53FA54D.lm.lmig.com>
2017-06-17 17:33:55 -0400

    pushing changes

--- web/src/main/groovy/com/fusionpower/controller/CrawlerController.groovy
+++ web/src/main/groovy/com/fusionpower/controller/CrawlerController.groovy
@@ -2,40 +2,38 @@ package com.fusionpower.controller
 
 import com.FbDataCollector.MyFbData
 import com.FbDataCollector.SimpleFbApiCall
-import com.fusionpower.crawler.Crawler
 import com.fusionpower.model.UserData
-import edu.uci.ics.crawler4j.crawler.CrawlConfig
-import edu.uci.ics.crawler4j.crawler.CrawlController
-import edu.uci.ics.crawler4j.fetcher.PageFetcher
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer
+import com.restfb.types.Page
+import groovy.json.JsonSlurper
 import org.springframework.amqp.core.AmqpTemplate
 import org.springframework.amqp.core.Queue
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.core.io.ClassPathResource
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CrawlerController {
-    @Autowired private AmqpTemplate amqpTemplate
-    @Autowired private Queue rabbitQueue
+    @Autowired
+    private AmqpTemplate amqpTemplate
+    @Autowired
+    private Queue rabbitQueue
 
 
     @RequestMapping("/crawl")
     def index(@RequestBody String uriToCrawl) {
         def url = new URL(uriToCrawl)
         if ((new URL(uriToCrawl)).host.toLowerCase().contains('facebook')) {
-            SimpleFbApiCall fbApi = new SimpleFbApiCall()
+            //SimpleFbApiCall fbApi = new SimpleFbApiCall()
             MyFbData myFbData = new MyFbData()
-            def company = url.path.split('/')[-1]
-            myFbData.setDataFromPage(fbApi.grabFBPageInfo(company))
-            myFbData.setLocations(fbApi.grabFbPageLocations(company))
+            //def company = url.path.split('/')[-1]
+            myFbData.setDataFromPage(new JsonSlurper().parse(new ClassPathResource('cocacola.json').file) as Page)
+            //myFbData.setLocations(fbApi.grabFbPageLocations(company))
             myFbData
         } else {
             amqpTemplate.convertAndSend(rabbitQueue.getName(), uriToCrawl)
+
             buildMockUserData(uriToCrawl)
         }
 
