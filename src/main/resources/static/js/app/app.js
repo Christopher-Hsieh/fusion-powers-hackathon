7624d0e8ce9d493cd277fd9e4e8b94f475b17c84
Leo <leo.guinan@cardinalhealth.com>
2017-06-17 11:22:05 -0400

    changing root context

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('crudApp',['ui.router','ngStorage']);
 
 app.constant('urls', {
-    BASE: 'http://localhost:8080/SpringBootCRUDApp',
-    USER_SERVICE_API : 'http://localhost:8080/SpringBootCRUDApp/api/user/'
+    BASE: 'http://localhost:8080/fusionpower',
+    USER_SERVICE_API : 'http://localhost:8080/fusionpower/api/user/'
 });
 
 app.config(['$stateProvider', '$urlRouterProvider',
