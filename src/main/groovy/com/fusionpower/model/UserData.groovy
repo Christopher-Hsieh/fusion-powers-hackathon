79aa48d98165033f1f30900c5f50a739b38d9257
Leo <leo.guinan@cardinalhealth.com>
2017-06-17 09:29:58 -0400

    Migrating crawler gradle project into existing maven app

---
+++ src/main/groovy/com/fusionpower/model/UserData.groovy
@@ -0,0 +1,25 @@
+package com.fusionpower.model
+
+class UserData {
+    List keywords
+    int numberOfBusinessLocations
+    Date businessEstablished
+    String businessZipCode
+    String legalEntity
+    String website
+    String businessName
+    String dba
+    String businessStreetAddress
+    String city
+    String stateCode
+    String zipCode
+    String firstName
+    String lastName
+    String phoneNumber
+    String email
+    boolean businessLocationOwned
+    boolean nonProfit
+    int numberOfficers
+    int numberWorkers
+
+}
